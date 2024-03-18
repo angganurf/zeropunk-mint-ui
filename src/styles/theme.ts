26d49de57c8d8bd487d6dc03544d76eace99401e
Angganurf <angganurfaizal234@gmail.com>
2024-03-18 17:15:55 +0700

    First Commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const color = {
+  primary: "#ffffff",
+  secondary: "#212121",
+  secondaryLight: "#C13541",
+  black: "#0A0A0B",
+  white: "#F8F7FF",
+  whiteShade: "#FFFFFF",
+  bg: "#121212",
+};
